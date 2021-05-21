147f58c5f478be003e1eadf254b240a2a1c41006
Corpse <68551308+CorpseH@users.noreply.github.com>
2021-05-21 18:42:11 +0530

    Add files via upload

---
+++ S2.js
@@ -0,0 +1,40 @@
+function Choose1() { 
+	
+    window.location = "PlusUltra.html";
+}
+function Choose2() { 
+	
+    window.location = "CypherRap.html";
+}
+function Choose3() { 
+	
+    window.location = "MadHouse.html";
+}
+function Choose4() { 
+	
+    window.location = "KnockKnock.html";
+}
+function Choose5() { 
+	
+    window.location = "AfraidOfMyself.html";
+}
+function Choose6() { 
+	
+    window.location = "AThousandReasons.html";
+}
+function Choose7() { 
+	
+    window.location = "Legend.html";
+}
+function Choose8() { 
+	
+    window.location = "NoHero.html";
+}
+function back() { 
+	
+    window.location = "index.html";
+}
+function Choose9() { 
+	
+    window.location = "ParticleArts.html";
+}
